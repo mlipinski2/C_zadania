8d72f90a53676d61b2e649aee2168f5b2dc46ac8
mlipinski2 <mlipinski2@sigma.ug.edu.pl>
2013-11-23 20:52:47 +0100

    Update zadanie7.c
    poprawki

--- 1_Budowanie programów w języku C/zadanie7.c
+++ 1_Budowanie programów w języku C/zadanie7.c
@@ -12,11 +12,10 @@ printf("odwrotność 0 z %.2lf nie istnieje\n");
 z=sqrt(x);
 printf("Pierwiastek kwadratowy z %.2lf wynosi:", z);
 } else if (x < 0) {
-printf("Pierwiastek
-r=1/x;
+printf("Pierwiastek", r=1/x;
 printf("odwrotność liczby %.2lf wynosi:", x);
 
-printf("Pierwiastek %.2lf
+printf("Pierwiastek", %.2lf;
 }
  
  
